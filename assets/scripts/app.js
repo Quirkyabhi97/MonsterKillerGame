1c700f01c878a834113aceecfc9528a7dc2202b1
Abhinav Sharma <abhi28121997@gmail.com>
2023-05-06 03:14:38 +0530

    Heal Functionality added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 const PLAYER_ATTACK_VALUE = 10;
-const PLAYER_STRONG_ATTACK_VALUE = 20;
+const PLAYER_STRONG_ATTACK_VALUE = 15;
 const MONSTER_ATTACK_VALUE = 15;
+const PLAYER_HEAL_VALUE = 20;
 
 //Global variable hence declared in CAPS
 
@@ -10,36 +11,55 @@ let currentMonsterLife = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-const attackMonster = (typeOfAttack) =>{
- if(typeOfAttack==='attack'){
+const endRound = () => {
+  let playerdamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerLife -= playerdamage;
+  console.log("players health" + currentPlayerLife);
+  console.log("monsters health" + currentMonsterLife);
+
+  if (currentMonsterLife <= 0 && currentPlayerLife > 0) {
+    alert("You Won");
+  } else if (currentPlayerLife <= 0 && currentMonsterLife > 0) {
+    alert("You Lost");
+  } else if (currentPlayerLife <= 0 && currentMonsterLife <= 0) {
+    alert("It's a draw");
+  }
+};
+
+const attackMonster = (typeOfAttack) => {
+  if (typeOfAttack === "attack") {
     let monsterdamage = dealMonsterDamage(PLAYER_ATTACK_VALUE);
     currentMonsterLife -= monsterdamage;
- }
- else if(typeOfAttack==='strong attack'){
+  } else if (typeOfAttack === "strong attack") {
     let monsterdamage = dealMonsterDamage(PLAYER_STRONG_ATTACK_VALUE);
     currentMonsterLife -= monsterdamage;
- }
-
- let playerdamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
- currentPlayerLife -= playerdamage;
-
- if (currentMonsterLife <= 0 && currentPlayerLife > 0) {
-   alert("You Won");
- } else if (currentPlayerLife <= 0 && currentMonsterLife > 0) {
-   alert("You Lost");
- } else if (currentPlayerLife <= 0 && currentMonsterLife <= 0) {
-   alert("It's a draw");
- }
-
-}
+  }
+  endRound();
+};
 
 const attackHandler = () => {
- attackMonster("attack");
+  attackMonster("attack");
 };
 
 const strongAttackHandler = () => {
- attackMonster("strong attack");
+  attackMonster("strong attack");
+};
+
+const healPlayerHandler = () => {
+  let finalHealValue;
+  if (currentPlayerLife>=chosenMaxLife - PLAYER_HEAL_VALUE){
+    alert("Heal Value modified as you cannot heal more than your max health");
+    finalHealValue = chosenMaxLife - currentPlayerLife
+  }
+  else {
+    finalHealValue = PLAYER_HEAL_VALUE
+  }
+  console.log("heal value is" + finalHealValue);
+  increasePlayerHealth(finalHealValue);
+  currentPlayerLife += finalHealValue;
+  endRound();
 };
 
 attackBtn.addEventListener("click", attackHandler);
 strongAttackBtn.addEventListener("click", strongAttackHandler);
+healBtn.addEventListener("click", healPlayerHandler);
